f5e02afaf133b2f553e96285a873edb6913f90cd
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-03-19 20:24:31 +0800

    membuat api dengan express

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import http from 'http';
+import express from 'express';
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'openapi' });
+const app = express();
 
-const server = http.createServer((req, res) => {
-  db.query('SELECT * FROM users', (err, result) => {
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM user', (err, result) => {
     if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
+      res.status(500).send('Internal Server Error');
       return;
     }
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(result));
+    res.json(result);
   });
 });
-server.listen(3000, () => console.log('server berjalan di port 3000!'));
+app.listen(3000, () => console.log('server berjalan di port 3000!'));

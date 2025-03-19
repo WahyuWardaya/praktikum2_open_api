71342b7198b62a36587a23ffd10565360d3abc87
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-03-19 18:25:22 +0800

    koneksi database mysql pada node js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import http from 'http';
+import mysql from 'mysql2';
+
+const db = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'openapi' });
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hellow world!');
+  db.query('SELECT * FROM users', (err, result) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  });
 });
 server.listen(3000, () => console.log('server berjalan di port 3000!'));
